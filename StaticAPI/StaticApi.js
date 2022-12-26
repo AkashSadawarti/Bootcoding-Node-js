7a01bdc37e4c1d66735c47c6f2d157f12550b891
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-26 11:32:00 +0530

    Static API with JSON Format

--- StaticAPI/StaticApi.js
+++ StaticAPI/StaticApi.js
@@ -1,8 +1,8 @@
 let http = require('http');
 //const { json } = require('stream/consumers');
-const data = require('./Info');
+const info = require('./Info');
 http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type' : 'json'})
-    res.write(JSON.stringify(data))
+    res.write(JSON.stringify(info))
     res.end()
 }).listen(1101)
