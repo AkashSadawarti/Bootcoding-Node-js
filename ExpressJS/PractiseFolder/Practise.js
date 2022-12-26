21646617c4699605fb8f80d1a4e0081b3d6d4949
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-26 11:29:34 +0530

    ExpressJS API Terminal and Postman testing

---
+++ ExpressJS/PractiseFolder/Practise.js
@@ -0,0 +1,31 @@
+// var express = require('express');
+// let app = express();
+
+// app.get('/route',(req,res) => {
+//     console.log('Request Indentified !');
+//      res.send('Hello World')
+// }).listen(9001)
+
+
+
+let express = require('express')
+let app = express()
+var router = express.Router();
+let count =0;
+router.get('/AdmissionsOk',(req,res)=>{
+    count++;
+    console.log('Welcome for Admissions');
+    res.send('Your Admissions is in Process')
+}).get('/AdmissionsNo',(req,res) =>{
+    count--;
+    console.log('Your Admisssion is Cancelled...!')
+    res.send('Tula Admission tasahi dete kon')     
+}).get('/countAdmissions',(req,res)=>{
+    console.log('Admission Status');
+    res.send('Total Admissions :' + count)
+})
+
+app.listen(9001,()=>{
+    console.log('Admissions are on')
+})
+app.use(router);
