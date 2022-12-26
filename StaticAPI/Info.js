d09723c18f224cb2023cd23719d4d8777c1e5479
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-26 11:34:13 +0530

    Update Static API

--- StaticAPI/Info.js
+++ StaticAPI/Info.js
@@ -1,3 +1,4 @@
+//Info to be revealed on Browser in JSON format.
 const data = [
     {name: 'Akash', email: 'asdkjh@gmail.com', mobno : '98879988' }
 ]
--- StaticAPI/StaticApi.js
+++ StaticAPI/StaticApi.js
@@ -1,6 +1,6 @@
 let http = require('http');
 //const { json } = require('stream/consumers');
-const info = require('./Info');
+const info = require('./Info'); //Here var info consist of path info.js which holds info
 http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type' : 'json'})
     res.write(JSON.stringify(info))
