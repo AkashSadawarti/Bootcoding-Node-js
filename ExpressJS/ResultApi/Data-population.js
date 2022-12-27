e39b7f312c0a728dc5b80ed5d7b89c7591187d2a
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-27 15:21:44 +0530

    Data population of Result project

---
+++ ExpressJS/ResultApi/Data-population.js
@@ -0,0 +1,26 @@
+function dataPopulation() {
+    let Data = [
+        {
+            name: "Oliver",
+            rollno: 1,
+            physics: 75,
+            Chemistry: 75,
+            maths: 75
+        }, {
+            name: "Mark",
+            rollno: 2,
+            physics: 85,
+            Chemistry: 65,
+            maths: 55
+        }, {
+            name: "John Doe",
+            rollno: 3,
+            physics: 75,
+            Chemistry: 75,
+            maths: 75
+        },
+    ]
+    return Data;
+    for(let d in )
+}
+console.log(dataPopulation());
