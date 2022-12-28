9992788bd5b8b4f64d30ce2d44775118563d889a
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-28 14:33:00 +0530

    Bubble Sort

--- sorting/BubbleSort.js
+++ sorting/BubbleSort.js
@@ -4,6 +4,7 @@ function getBubblesort(Arr) {
    for (let i = 0; i <= Arr.length; i++) {
       for (let j = i + 1; j < Arr.length; j++) {
          if(Arr[j]< Arr[i]){
+            //swapping logic
             [Arr[j],Arr[i]] = [Arr[i],Arr[j]]
          }
       }
@@ -13,3 +14,4 @@ function getBubblesort(Arr) {
 }
 
 console.log(getBubblesort([5, 2, 3, 4, 7, 1]));
+
