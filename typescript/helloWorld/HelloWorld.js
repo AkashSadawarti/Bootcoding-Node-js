ff5691c9b41ac5da1f1b61dcba132ee639cf33aa
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-01-03 12:53:05 +0530

    HelloWorld TS

---
+++ typescript/helloWorld/HelloWorld.js
@@ -0,0 +1,5 @@
+var FirstName = 'Alex';
+var LastName = 'Griffin';
+var age = 100;
+var info = FirstName + LastName + " " + age;
+console.log(info);
---
+++ typescript/helloWorld/HelloWorld.ts
@@ -0,0 +1,6 @@
+
+const FirstName : string = 'Alex';
+const LastName : string = 'Griffin';
+let age : number = 100
+let info : any = FirstName+ LastName + " " + age;
+console.log(info);
