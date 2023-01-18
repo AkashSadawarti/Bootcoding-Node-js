8e17ff26328c3dccd902a3ed0062265f24c8fedb
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-01-18 16:16:43 +0530

    student profile model

---
+++ ExpressJS/models/student-profile.ts
@@ -0,0 +1,16 @@
+export class StudentProfile{
+    id : bigint;
+    gender : string;
+    college : string;
+    degreeName : string;
+    experienceLevel : string;
+    companyName : string;
+    profilePicture : URL;
+    DOB : Date;
+    githubURL : URL;
+    linkedlnURL : URL;
+    createDate : Date;
+    createdBy : string;
+    modifiedDate : Date;
+    modifiedBy : string;
+}
