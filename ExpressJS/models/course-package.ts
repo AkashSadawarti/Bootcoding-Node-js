c530d52ccca0c5424efb498aaf4a4ca698cdd5db
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-01-18 15:55:17 +0530

    course Package model

---
+++ ExpressJS/models/course-package.ts
@@ -0,0 +1,9 @@
+export class CoursePackage {
+    id: bigint;
+    packageName: string;
+    packageDuration: string;
+    createDate: Date;
+    createdBy: string;
+    modifiedDate: Date;
+    modifiedBy: string;
+}
