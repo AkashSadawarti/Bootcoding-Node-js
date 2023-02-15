c7d84f6c02ebaea0337c8b8da157983f93ba5aed
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-15 15:21:39 +0530

    bird Watcher Excercism

---
+++ bird-watcher.js
@@ -0,0 +1,14 @@
+// nstructions
+// You're an avid bird watcher that keeps track of how many birds have visited your garden in the last seven days.
+
+// You have six tasks, all dealing with the numbers of birds that visited your garden.
+
+// 1. Check what the counts were last week
+// For comparison purposes, you always keep a copy of last week's counts nearby, which were: 0, 2, 5, 3, 7, 8 and 4. Implement the BirdWatcher.getLastWeek() method that returns last week's counts:
+let birdsPerDay1 = [0,2,5,3,7,8,4]
+function getLastWeek(){
+       return birdsPerDay1;
+}
+console.log("---------Answer 1---------------")
+console.log(getLastWeek())
+// BirdWatcher.getLastWeek();
